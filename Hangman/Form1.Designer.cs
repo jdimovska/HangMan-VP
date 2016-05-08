9741513ad537541157f2aecb9084768edde59704
Jona Dimovska <jona.dimovska@hotmail.com>
2016-05-08 23:39:52 +0200

    bugs

--- Hangman/Form1.Designer.cs
+++ Hangman/Form1.Designer.cs
@@ -44,9 +44,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(249, 144);
+            this.button1.Location = new System.Drawing.Point(332, 177);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 35);
+            this.button1.Size = new System.Drawing.Size(161, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "Play";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,9 +55,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(249, 185);
+            this.button2.Location = new System.Drawing.Point(332, 228);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 34);
+            this.button2.Size = new System.Drawing.Size(161, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Help";
             this.button2.UseVisualStyleBackColor = true;
@@ -64,9 +66,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(249, 226);
+            this.button3.Location = new System.Drawing.Point(332, 278);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 34);
+            this.button3.Size = new System.Drawing.Size(161, 42);
             this.button3.TabIndex = 2;
             this.button3.Text = "High scores";
             this.button3.UseVisualStyleBackColor = true;
@@ -76,9 +79,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(61, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(81, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(498, 87);
+            this.pictureBox1.Size = new System.Drawing.Size(664, 107);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -86,9 +90,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(249, 144);
+            this.button4.Location = new System.Drawing.Point(332, 177);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(121, 35);
+            this.button4.Size = new System.Drawing.Size(161, 43);
             this.button4.TabIndex = 4;
             this.button4.Text = "FINKI";
             this.button4.UseVisualStyleBackColor = true;
@@ -97,9 +102,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(249, 185);
+            this.button5.Location = new System.Drawing.Point(332, 228);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(121, 35);
+            this.button5.Size = new System.Drawing.Size(161, 43);
             this.button5.TabIndex = 5;
             this.button5.Text = "Capital cities";
             this.button5.UseVisualStyleBackColor = true;
@@ -108,9 +114,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(330, 225);
+            this.button6.Location = new System.Drawing.Point(332, 277);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(121, 35);
+            this.button6.Size = new System.Drawing.Size(161, 43);
             this.button6.TabIndex = 6;
             this.button6.Text = "Animals";
             this.button6.UseVisualStyleBackColor = true;
@@ -123,9 +130,10 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Modern No. 20", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Snow;
-            this.label2.Location = new System.Drawing.Point(12, 305);
+            this.label2.Location = new System.Drawing.Point(27, 149);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(603, 192);
+            this.label2.Size = new System.Drawing.Size(777, 232);
             this.label2.TabIndex = 9;
             this.label2.Text = "Rules:\r\n1) Choose a category \r\n2) Choose the length of the word\r\n3) Start guessin" +
     "g the word\r\n\r\nThe score you get depends on the length of the word you choose.\r\n\r" +
@@ -137,9 +145,10 @@
             // 
             this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
-            this.pictureBox9.Location = new System.Drawing.Point(576, 305);
+            this.pictureBox9.Location = new System.Drawing.Point(768, 375);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(46, 39);
+            this.pictureBox9.Size = new System.Drawing.Size(61, 48);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox9.TabIndex = 45;
             this.pictureBox9.TabStop = false;
@@ -147,10 +156,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(631, 356);
+            this.ClientSize = new System.Drawing.Size(841, 438);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button6);
@@ -161,6 +170,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "The hangman game";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
